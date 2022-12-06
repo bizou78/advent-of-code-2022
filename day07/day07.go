468f9936d8ad19ccd1ad76ae9d3d3fbeef168a04
b.jaeger <b.jaeger@groupeonepoint.com>
2022-12-06 12:00:44 +0100

    Prepare day07

---
+++ day07/day07.go
@@ -0,0 +1,15 @@
+package day07
+
+func part1(input string) (int, error) {
+
+	var result int
+
+	return result, nil
+}
+
+func part2(input string) (int, error) {
+
+	var result int
+
+	return result, nil
+}
